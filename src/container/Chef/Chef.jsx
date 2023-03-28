28ff79ad0d47742469c52806bfbeb91f66b0768f
Arinze-Kevin <egboguarinze@gmail.com>
2023-03-28 22:02:36 +0100

    Added chef section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -8,6 +8,28 @@ const Chef = () => (
     <div className='app__wrapper_img app__wrapper_img-reverse'>
       <img src={images.chef} alt="chef" />
     </div>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chef's Word" />
+      <h1 className='headtext__cormorant'>What we believe in</h1>
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img src={images.quote} alt="quote" />
+          <p className='p__opensans'>
+            Lorem ipsum Dolor Sit Amet. Consectetur Adipiscing Elit Auctor Sit.
+          </p>
+        </div>
+        <p className='p__opensans'>
+          Auctor Sit laculls In Arcu. Vulputate Nulla Lobortis Mauris Eget Sit.
+          Nulla Scelerisque Scelerisque Congue Ac Consequat. Allquam Molestie Lectus Eu.
+          Congue laculis Integer Curabitur Semper Sit Nunc.
+        </p>
+      </div>
+      <div className='app__chef-sign'>
+        <p>Kevin Luo</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
   </div>
 );
 
